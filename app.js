a23ee4b34fe1c77b637cf4e0e90a322f0f82123f
yongmin Lee <nogada@gmail.com>
2015-12-25 14:09:02 +0900

    express 설치

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req,res) {
-	res.send('hello world');
-});
+app.use(express.static(__dirname + "/public"));
 
-var server = app.listen(3000, function() {
-	var host = server.address().address;
-	var port = server.address().port;
-
-	console.log('listen at http://%s:%s', host, port);
-});
+app.listen(3000);
+console.log("server running on port 3000");
